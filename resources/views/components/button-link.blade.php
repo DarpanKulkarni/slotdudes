442569ba39bbb394ed8518afb9504ffec3d7d1e8
DarpanCodes <contact@darpan.co>
2025-12-26 10:36:52 +0530

    update button colors

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -6,7 +6,7 @@
 
 @php
     $sizeClasses = [
-        'sm' => 'px-4 py-1.5 text-xs',
+        'sm' => 'px-4 py-2 text-xs',
         'default' => 'px-8 py-2 text-sm',
         'lg' => 'px-8 py-2 text-lg',
         'xl' => 'px-8 md:px-14 py-4 text-lg md:text-2xl'
@@ -21,6 +21,8 @@
         'secondary-outline' => 'border-2 border-secondary-500 text-secondary-500 hover:bg-secondary-500 hover:text-white focus-visible:ring-secondary-600',
         'accent' => 'bg-accent-500 hover:bg-accent-600 focus-visible:ring-accent-600 text-white',
         'accent-outline' => 'border-2 border-accent-500 text-accent-500 hover:bg-accent-500 hover:text-white focus-visible:ring-accent-600',
+        'green' => 'bg-green-500 hover:bg-green-600 focus-visible:ring-green-600 text-white',
+        'green-outline' => 'border-2 border-green-500 text-green-500 hover:bg-green-500 hover:text-white focus-visible:ring-green-600',
     ];
 
     $baseClasses = 'inline-flex items-center justify-center text-center rounded-full uppercase font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 transition-all duration-300 disabled:opacity-50 disabled:cursor-not-allowed';
--- resources/views/livewire/casino-list.blade.php
+++ resources/views/livewire/casino-list.blade.php
@@ -11,25 +11,27 @@
                 <img src="{{ $casino->getFirstMediaUrl('logos', 'thumb') }}" alt="{{ $casino->name . ' logo' }}" class="w-full">
 
                 <div
-                    class="absolute flex flex-col items-center justify-center inset-0 p-4 gap-4 bg-primary-600 transition-opacity duration-300
+                    class="absolute flex flex-col items-center justify-center inset-0 p-4 gap-2 transition-opacity duration-300
                            opacity-0 pointer-events-none lg:group-hover:opacity-100 lg:group-hover:pointer-events-auto"
                     :class="open ? 'opacity-100! pointer-events-auto!' : ''"
                 >
-                    <x-button-link href="{{ route('casino.detail', $casino->slug) }}" variant="white-outline" class="w-full">
-                        Read More
+                    <x-button-link href="{{ $casino->link }}" variant="green" class="w-full">
+                        Visit Casino
                         <span><x-icons.chevron-right class="w-4 h-4 ms-2"/></span>
                     </x-button-link>
 
-                    <x-button-link href="{{ $casino->link }}" variant="white-outline" class="w-full">
-                        Visit Casino
+                    <x-button-link href="{{ route('casino.detail', $casino->slug) }}" variant="secondary" class="w-full">
+                        Read More
                         <span><x-icons.chevron-right class="w-4 h-4 ms-2"/></span>
                     </x-button-link>
                 </div>
             </div>
         @endforeach
+    </x-layouts.container>
 
+    <x-layouts.container>
         @if($this->casinos->count() < $this->totalCasinos)
-            <div x-intersect.full="$wire.loadMore()" class="w-full flex justify-center items-center mt-8">
+            <div x-intersect.full="$wire.loadMore()" class="w-full flex justify-center items-center pb-12">
                 <x-icons.loader wire:loading wire:target="loadMore" class="w-4 h-4 animate-spin" /> <span class="text-sm text-secondary-600 ms-2">Loading...</span>
             </div>
         @endif
