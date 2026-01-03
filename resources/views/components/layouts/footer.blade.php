5594e60914f0be83a53739b94d191835fbdd1d1e
DarpanKulkarni <kulkarnidarpan007@gmail.com>
2026-01-03 11:34:12 +0530

    add footer logos

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -4,6 +4,18 @@
     </x-layouts.container>
 </section>
 
-<footer class="bg-primary-600 text-white/70 text-sm text-center py-6">
-    {!! app(\App\Settings\SiteSettings::class)->copyrightText !!}
+<footer class="bg-primary-600 text-white/70 text-sm text-center py-6 space-y-4">
+    <div class="flex items-center justify-center gap-6">
+        <a href="https://instagram.com/slotdudes_com" target="_blank" rel="nofollow">
+            <img src="{{ asset('/images/cd-instagram-logo.webp') }}" alt="instagram logo" class="w-8">
+        </a>
+
+        <a href="https://www.gambleaware.org/" target="_blank" rel="nofollow">
+            <img src="{{ asset('/images/cd-gamble-aware-logo.webp') }}" alt="gamble aware logo" class="w-40">
+        </a>
+    </div>
+
+    <div>
+        {!! app(\App\Settings\SiteSettings::class)->copyrightText !!}
+    </div>
 </footer>
