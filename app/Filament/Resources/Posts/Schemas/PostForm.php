8d12137e2555ae8322aa94603be3ce5ddd212c67
DarpanKulkarni <kulkarnidarpan007@gmail.com>
2026-01-04 12:22:01 +0530

    remove block

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -35,10 +35,6 @@ class PostForm
                         RichEditor::make('content')
                             ->rules(['required'])
                             ->markAsRequired()
-                            ->customBlocks([
-                                ButtonBlock::class,
-                                ImageBlock::class,
-                            ])
                             ->toolbarButtons([
                                 ['bold', 'italic', 'underline', 'strike', 'link'],
                                 ['h2', 'h3'],
