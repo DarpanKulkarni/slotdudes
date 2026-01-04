7af30485cb93d9444aa84b5b3ad78410848be5a4
DarpanKulkarni <kulkarnidarpan007@gmail.com>
2026-01-04 10:26:11 +0530

    add 18+ logo

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -13,6 +13,10 @@
         <a href="https://www.gambleaware.org/" target="_blank" rel="nofollow">
             <img src="{{ asset('/images/cd-gamble-aware-logo.webp') }}" alt="gamble aware logo" class="w-40">
         </a>
+
+        <a href="https://instagram.com/slotdudes_com" target="_blank" rel="nofollow">
+            <img src="{{ asset('/images/cd-18-plus-logo.webp') }}" alt="18+ logo" class="w-8">
+        </a>
     </div>
 
     <div>
