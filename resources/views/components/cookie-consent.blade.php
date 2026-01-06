51b13b8d5a76929c00c3893e7dfbb0c18e6663cf
DarpanKulkarni <kulkarnidarpan007@gmail.com>
2026-01-06 19:51:21 +0530

    Update cookie consent popup

--- resources/views/components/cookie-consent.blade.php
+++ resources/views/components/cookie-consent.blade.php
@@ -1,25 +1,22 @@
 @once
-    <div x-data="cookieConsent" x-show="show" x-cloak
-        class="fixed inset-0 z-50 flex items-center justify-center bg-black/90">
-        <div class="w-full max-w-lg p-6 mx-4 bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700">
-            <h5 class="text-2xl font-semibold text-gray-900 dark:text-white">Cookie Consent</h5>
-            <p class="mt-2 text-sm text-gray-600 dark:text-gray-400">
-                This website uses cookies to ensure you get the best experience. Here are the details of the cookies we use:
-            </p>
-            <div class="mt-4 space-y-2">
-                <div class="space-y-1 p-3 bg-gray-100 rounded-lg dark:bg-gray-700">
-                    <h6 class="text-base font-semibold text-gray-900 dark:text-white">Essential Cookies</h6>
-                    <p class="text-sm text-gray-600 dark:text-gray-400">These cookies are necessary for the website to function and cannot be switched off in our systems.</p>
-                </div>
-                <div class="space-y-1 p-3 bg-gray-100 rounded-lg dark:bg-gray-700">
-                    <h6 class="text-base font-semibold text-gray-900 dark:text-white">Analytics Cookies</h6>
-                    <p class="text-sm text-gray-600 dark:text-gray-400">These cookies allow us to count visits and traffic sources so we can measure and improve the performance of our site.</p>
-                </div>
-                <div class="space-y-1 p-3 bg-gray-100 rounded-lg dark:bg-gray-700">
-                    <h6 class="text-base font-semibold text-gray-900 dark:text-white">Marketing Cookies</h6>
-                    <p class="text-sm text-gray-600 dark:text-gray-400">These cookies may be set through our site by our advertising partners to build a profile of your interests and show you relevant adverts on other sites.</p>
+    <div x-data="cookieConsent" x-show="show" x-cloak class="fixed inset-0 z-50 flex items-center justify-center">
+        <div class="w-full max-w-lg  p-6 mx-4 bg-white border border-gray-200 rounded-lg shadow-lg">
+            <h5 class="text-2xl font-semibold mb-4">Cookie Consent</h5>
+            <div class="overflow-y-auto h-50">
+                <p class="mt-2 text-sm">This website uses cookies to ensure you get the best experience. Here are the details of the cookies we use:</p>
+
+                <div class="mt-4 space-y-2">
+                    <h6 class="text-base font-semibold">Essential Cookies</h6>
+                    <p class="text-sm">These cookies are necessary for the website to function and cannot be switched off in our systems.</p>
+
+                    <h6 class="text-base font-semibold">Analytics Cookies</h6>
+                    <p class="text-sm">These cookies allow us to count visits and traffic sources so we can measure and improve the performance of our site.</p>
+
+                    <h6 class="text-base font-semibold">Marketing Cookies</h6>
+                    <p class="text-sm">These cookies may be set through our site by our advertising partners to build a profile of your interests and show you relevant adverts on other sites.</p>
                 </div>
             </div>
+
             <div class="grid grid-cols-2 gap-4 mt-6">
                 <x-button-link @click="accept" tag="button">Accept</x-button-link>
                 <x-button-link @click="reject" tag="button" variant="secondary">Deny</x-button-link>
