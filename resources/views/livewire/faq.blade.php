dfaaba682dc940161aaefe63810481406659bbb6
DarpanKulkarni <kulkarnidarpan007@gmail.com>
2026-01-28 12:09:16 +0530

    fix faq bug

--- resources/views/livewire/faq.blade.php
+++ resources/views/livewire/faq.blade.php
@@ -1,31 +1,36 @@
 <div class="space-y-8">
     <div class="prose prose-lg">
-        <h2 class="text-black dark:text-white">Frequently Asked Questions</h2>
+        <h2 class="text-black">Frequently Asked Questions</h2>
     </div>
 
-    <div class="w-full border-y border-gray-200 dark:border-gray-700 divide-y divide-gray-200 dark:divide-gray-700">
+    <div class="w-full border-y border-gray-200 divide-y divide-gray-200" x-data="{ activeAccordion: null }">
         @foreach ($faqs as $faq)
-            <div x-data="{ expanded: false }" class="w-full">
+            <div class="w-full">
                 <button
                     type="button"
-                    @click="expanded = !expanded"
-                    class="flex w-full items-center justify-between py-2 text-left focus:outline-none group"
-                    :class="expanded ? 'text-primary-600 dark:text-primary-500' : 'text-gray-900 dark:text-white'"
+                    @click="activeAccordion = activeAccordion === {{ $faq->id }} ? null : {{ $faq->id }}"
+                    class="flex w-full items-center justify-between py-2 text-left focus:outline-none group text-gray-900"
+                    :class="{ '!text-primary-600': activeAccordion === {{ $faq->id }} }"
                 >
-                    <span class="text-lg font-medium leading-7 flex items-center min-h-21 md:min-h-0 md:py-2">
+                    <span class="text-lg font-medium leading-7 flex items-center min-h-[5.25rem] md:min-h-0">
                         {{ $faq->question }}
                     </span>
                     <span class="ml-6 flex items-center">
-                        <svg x-show="!expanded" class="h-6 w-6 transform transition-transform duration-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                        <svg
+                            class="h-6 w-6 transform transition-transform duration-300"
+                            :class="{ 'rotate-180': activeAccordion === {{ $faq->id }} }"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                        >
                             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                        </svg>
-                        <svg x-show="expanded" x-cloak class="h-6 w-6 transform transition-transform duration-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
                         </svg>
                     </span>
                 </button>
-                <div x-show="expanded" x-collapse x-cloak class="pb-4">
-                    <div class="text-base leading-7 text-gray-600 dark:text-gray-400 prose dark:prose-invert max-w-none">
+                <div
+                    x-show="activeAccordion === {{ $faq->id }}"
+                    x-collapse.duration.300ms
+                    x-cloak
+                >
+                    <div class="pb-4 text-base leading-7 text-gray-600 prose max-w-none">
                         {!! $faq->answer !!}
                     </div>
                 </div>
